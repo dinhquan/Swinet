859b2be1f86fc3953c917ec008dc41f4991eae4d
Quan <quannd2@smartosc.com>
2021-10-19 11:43:29 +0700

    Add files

---
+++ Swinet/SwinetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwinetApp.swift
+//  Swinet
+//
+//  Created by Quan on 10/18/21.
+//
+
+import SwiftUI
+
+@main
+struct SwinetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
